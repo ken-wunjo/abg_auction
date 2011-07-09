af17bbe173c9c198dedc89a41a68789cdfe21b80
ken ernst <kernst@mobileposse.com>
2011-07-09 10:30:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbgAuction::Application.config.secret_token = '17bd068844754bc57b602aac41124efbf54a4a3f3221b8049cb08375f420bf13f46d09cd0ddc9d1fc6c19eb351589b6402e350020e420bea61a304353c4c1837'
